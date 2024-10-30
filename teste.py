9a7f54e359cf1f4fc50c205a1bc14206e8990dcc
Lucas-Finoti-Rodrigues <lucas.rodrigues@ciag.org.br>
2024-10-30 14:04:48 -0300

    feat: arquivo teste modificado

--- teste.py
+++ teste.py
@@ -9,7 +9,7 @@ model = YOLO('/home/ciag/Desktop/reconhecimentoFerrugemSoja/runs/detect/train9/w
 image_path = '/home/ciag/Desktop/reconhecimentoFerrugemSoja/val/images/ferrugem24.jpg'
 
 # Pasta de saída para salvar as imagens com detecção (opcional)
-output_dir = 'detections_output'
+output_dir = 'uploads/'
 os.makedirs(output_dir, exist_ok=True)
 
 def detect_and_save(image_path):
@@ -23,26 +23,20 @@ def detect_and_save(image_path):
     if result.boxes is not None and len(result.boxes) > 0:
         # Salva a imagem com as bounding boxes
         output_file = os.path.join(output_dir, os.path.basename(image_path))
-        result.plot(save=True, filename=output_file)
+        output_file2 = os.path.join('/home/ciag/Desktop/reconhecimentoFerrugemSoja/static/uploads/', os.path.basename(image_path))
+        result.plot(save=True, filename=output_file2)        
         
         # Carrega a imagem salva para exibir
-        img = cv2.imread(output_file)
-        cv2.imshow("Detecções", img)
-        cv2.waitKey(0)  # Aguarda até que uma tecla seja pressionada
-        cv2.destroyAllWindows()  # Fecha a janela de visualização
+        # img = cv2.imread(output_file)
+        # cv2.imshow("Detecções", img)
+        # cv2.waitKey(0)  # Aguarda até que uma tecla seja pressionada
+        # cv2.destroyAllWindows()  # Fecha a janela de visualização
 
         print(f"Detecção salva e exibida em: {output_file}")
     else:
         print("Nenhuma detecção encontrada na imagem.")
-
+    return output_file
+def detectImage(image_path):
+    if os.path.isfile(image_path):
+        return detect_and_save(image_path)
 # Verifica se o caminho é uma única imagem ou uma pasta de imagens
-if os.path.isfile(image_path):
-    detect_and_save(image_path)
-elif os.path.isdir(image_path):
-    # Se for um diretório, processa cada imagem na pasta
-    for img_file in os.listdir(image_path):
-        img_path = os.path.join(image_path, img_file)
-        if img_file.endswith(('.jpg', '.jpeg', '.png')):  # Verifica formatos de imagem
-            detect_and_save(img_path)
-else:
-    print("Caminho inválido. Certifique-se de fornecer uma imagem ou pasta de imagens.")
